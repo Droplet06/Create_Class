1cffa5d70e03012916d217db142711ac80061b44
Droplet06 <yanglingyuan.li@edu.sait.ca>
2023-01-25 20:18:45 -0700

    Initial commit

---
+++ Create_Class/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_Class
+{
+    internal class Person
+    {
+        private int personId;
+        public string firstName;
+        public string lastName;
+        private string favoriteColor;
+        private int age;
+        private bool isWorking;
+
+        public Person(int personId, string firstName, string lastName, string favoriteColor, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.favoriteColor = favoriteColor;
+            this.age = age;
+            this.isWorking = isWorking;
+        }
+
+        public void DisplayPersonInfo()
+        {
+            string firstName = this.firstName;
+            string lastName = this.lastName;
+          
+
+       
+
+            Console.WriteLine(personId + ":" + firstName + " " + lastName + 
+                "Brooks's favorite color is: " + favoriteColor);
+        }
+
+        public void ChangeFavoriteColor()
+        {
+            this.favoriteColor = "white";
+        }
+
+        public int GetAgeInTenYears() 
+        {
+            return this.age + 10;
+
+           
+
+        }
+
+        public void TheAge()
+        {
+            Console.WriteLine(firstName + " " + lastName + "'s Age in 10 years is: " + GetAgeInTenYears());
+        }
+
+        public void FormatList()
+        {
+            Console.WriteLine("PersonID: " + personId
+                + "\nFirstName: " + firstName
+                + "\nLastName: " + lastName
+                + "\nFavoriteColor: " + favoriteColor
+                + "\nAge: " + age
+                + "\nIsWorking: " + isWorking);
+        }
+    }
+}
---
+++ Create_Class/Program.cs
@@ -0,0 +1,51 @@
+﻿using Person.person1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1;
+
+            person1 = new Person(1, "lan", "Brooks", "Red", 30, true);
+
+            person1.ChangeFavoriteColor();
+
+            person1.DisplayPersonInfo();
+
+            Person person2;
+
+            person2 = new Person(2, "Gina", "James", "Green", 18, false);
+
+            person2.DisplayPersonInfo();
+
+
+            Person person3;
+                
+            person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+
+            person3.FormatList();
+
+            Person person4;
+
+            person4 = new Person(4, "Mary", "Beals", "Yellow", 28,true);
+
+            person4.TheAge();
+
+
+
+
+
+
+
+
+         
+        }
+    }
+}
